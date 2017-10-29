951f7ff849746ea71f47e074e680eb625d90ba71
akira_you <you.akira.noda@gmail.com>
2017-10-29 22:01:16 +0900

    first commit

---
+++ arduino/haptic/haptic.ino
@@ -0,0 +1,23 @@
+#define  LED 13 
+#define  VIVE 0
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+    pinMode(LED, OUTPUT);  
+  pinMode(VIVE, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+     int key=Serial.read();
+     if('a'<=key&& key<='z'){
+      int time=key-'a'+1;
+      digitalWrite(VIVE,HIGH);
+      digitalWrite(LED,HIGH);
+      delay(10*time);
+     }
+  }
+  digitalWrite(VIVE,LOW);
+  digitalWrite(LED,LOW);
+}
---
+++ unity/haptic/Assets/Slash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+public class Slash : MonoBehaviour {
+    public string portName = "COM4";
+    public int baudRate = 9600;
+    private SerialPort serialPort_;
+    int count = 0;
+    // Use this for initialization
+    void Start () {
+        serialPort_ = new SerialPort(portName, baudRate);
+        serialPort_.Open();
+
+    }
+    void air()
+    {
+        try
+        {
+            serialPort_.Write("l");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+    // Update is called once per frame
+    float timee = 0;
+    bool go = false;
+    void Update()
+    {
+        timee += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioSource audioSource= this.GetComponent<AudioSource>();
+            audioSource.Play();
+            go = true;
+            count = 10;
+            timee = 0;
+        }
+        if (go && timee>3)
+        {
+            go = false;
+                air();
+        }
+      
+    }
+}
